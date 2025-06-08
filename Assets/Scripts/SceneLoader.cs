05c4bb1072a591db9060cbe8549763505cfc95a4
PaPerman99 <Jinhaoyang1999@outlook.com>
2025-06-09 00:23:52 +0900

    临时UI和场景切换
    写了两个script 控制场景切换
    安装了 Textmeshpro
    加了三个临时场景

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    public void LoadSceneByIndex(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
